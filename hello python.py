64590eb5edcbc6478d8a5c32b5e265f4472c3019
nicolenelson10 <nicolenelson2028@cs.sjcetpalai.ac.in>
2024-10-01 14:52:17 +0530

    Add files via upload
    my 1st project

---
+++ hello python.py
@@ -0,0 +1,10 @@
+print("hello python")
+name=input("enter the student name:")
+roll_no=int(input("enter the roll no:"))
+cgpa=float(input("enter your cgpa:"))
+cgpa=cgpa*10
+roll_no=roll_no+1
+print("name of the student:",name)
+print("roll number:",roll_no)
+print("your percentage is:",cgpa,"%")
+
