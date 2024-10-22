9024b75a0f9ef3f5d05e1f49b83b92ad238107ae
nicolenelson10 <nicolenelson2028@cs.sjcetpalai.ac.in>
2024-10-22 15:07:10 +0530

    Add files via upload

---
+++ project.greatest.py
@@ -0,0 +1,14 @@
+'''
+author name:Nicole Nelson
+date:22-10-2024
+to find greatest of three numbers
+'''
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+num3=int(input("enter the third number:"))
+if(num1>num2 and num1>num3):
+    print("num1 is greatest")
+elif(num2>num3 and num2>num1):
+    print("num2 is greatest")
+else:
+    print("num3 is greatest")
