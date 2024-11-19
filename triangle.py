0e555ff43d9dd68e4b2b87f11771c2794d219000
nicolenelson10 <nicolenelson2028@cs.sjcetpalai.ac.in>
2024-11-19 15:59:23 +0530

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,9 @@
+'''
+author name:Nicole Nelson
+date:29-10-2024
+'''
+n=int(input("enter the  no of rows:"))
+for i in range(n):
+    for j in range(i+1):
+        print('*',end=" ")
+    print(" ")
